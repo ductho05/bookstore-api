bb64174a85ff4b72828501d3c5fd940737809a18
Phan Văn Đức Anh <93977845+aducanha@users.noreply.github.com>
2023-12-18 12:16:25 +0700

    Update flashsale.js

--- src/routes/flashsale.js
+++ src/routes/flashsale.js
@@ -19,30 +19,14 @@ if (isRun == true) {
     // local
     // rule.hour = 23
     // deloy
-    rule.hour = 5
+    rule.hour = 16
 
-    rule.minute = [0, 1, 2, 3, 4, 5, 6, 7, 8,  9,  10,  11,  12,  13,  14,  15,
-          16, 17, 18, 19, 20, 21, 22,  23,  24,  25,  26,  27,  28,  29, 30, 31,
-           32 ,33, 34, 35, 36, 37, 38, 39, 40, 41, 42 ,43, 44, 45, 46, 47, 48, 
-           49, 50, 51, 52 ,53, 54, 55, 56, 57, 58, 59]
+    rule.minute = 59
     rule.second = 0
     schedule.scheduleJob(rule, () => {
-        //FlashSaleController.addLoopSale()
-     
+        FlashSaleController.addLoopSale()
         console.log(new Date().getHours())
         console.log(new Date().getMinutes())
-    })
-
-    const rule3 = new schedule.RecurrenceRule()
-    // local
-    // rule.hour = 23
-    // deloy
-    rule3.hour = 0
-    rule3.minute = 14
-    rule3.second = 0
-    schedule.scheduleJob(rule3, () => {
-        //FlashSaleController.addLoopSale()
-        console.log('loop sale 0')
     })
 
     // Tạo một quy tắc định kỳ cho mỗi 3 tiếng
