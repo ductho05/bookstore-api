3935c4797f98484e966b2df18d0e4e8dc7e15022
Phan Văn Đức Anh <93977845+aducanha@users.noreply.github.com>
2024-03-17 12:56:21 +0700

    ducanmhj

--- src/services/EvaluateService.js
+++ src/services/EvaluateService.js
@@ -1,327 +1,275 @@
-const mongoose = require("mongoose")
-const Evaluate = require("../models/Evaluate")
-const ServiceResponse = require("../response/ServiceResponse")
-const Status = require("../utils/Status")
-const Messages = require("../utils/Messages")
-const EvaluateDTO = require("../dtos/EvaluateDTO")
-const User = require("../models/User")
-const Product = require("../models/Product")
+const mongoose = require("mongoose");
+const Evaluate = require("../models/Evaluate");
+const ServiceResponse = require("../response/ServiceResponse");
+const Status = require("../utils/Status");
+const Messages = require("../utils/Messages");
+const EvaluateDTO = require("../dtos/EvaluateDTO");
+const User = require("../models/User");
+const Product = require("../models/Product");
 
 class EvaluateService {
+  async getAll() {
+    try {
+      // console.log("EvaluateService.getAll");
 
-    async getAll() {
+      const data = await Evaluate.find()
+        .populate("product")
+        .populate("user")
+        .sort({ createdAt: -1 })
+        .exec();
 
-        try {
+      //console.log("EvaluateService.getAll", data);
+      const evaluateDToList = [];
+      data.forEach((evaluate) => {
+        const evaluateDTO = EvaluateDTO.mapEvaluateToEvaluateDTO(evaluate);
+        //console.log("EvaluateService.getAll", evaluateDTO);
+        evaluateDToList.push(evaluateDTO);
+      });
 
-            const data = await Evaluate.find()
-                .populate("product")
-                .populate("user")
-                .sort({ createdAt: -1 }).exec()
-
-            const evaluateDToList = []
-            data.forEach(evaluate => {
-
-                const evaluateDTO = EvaluateDTO.mapEvaluateToEvaluateDTO(evaluate)
-
-                evaluateDToList.push(evaluateDTO)
-            })
-
-            return new ServiceResponse(
-                200,
-                Status.SUCCESS,
-                Messages.GET_DATA_SUCCESS,
-                evaluateDToList
-            )
-        } catch (err) {
-
-            return new ServiceResponse(
-                500,
-                Status.ERROR,
-                Messages.INTERNAL_SERVER
-            )
-        }
+      return new ServiceResponse(
+        200,
+        Status.SUCCESS,
+        Messages.GET_DATA_SUCCESS,
+        evaluateDToList
+      );
+    } catch (err) {
+      //console.log(err);
+      return new ServiceResponse(500, Status.ERROR, Messages.INTERNAL_SERVER);
     }
+  }
 
-    async like(uid, evaluateId) {
+  async like(uid, evaluateId) {
+    try {
+      const evaluate = await Evaluate.findOne({ _id: evaluateId }).exec();
+      if (evaluate) {
+        const likes = evaluate.likes;
+        const foundLike = likes.find((l) => l.equals(uid));
 
-        try {
-
-            const evaluate = await Evaluate.findOne({ _id: evaluateId }).exec()
-            if (evaluate) {
-
-                const likes = evaluate.likes
-                const foundLike = likes.find(l => l.equals(uid))
-
-                if (foundLike) {
-
-                    const newLikes = likes.filter(l => !l.equals(uid))
-                    evaluate.likes = [...newLikes]
-                } else {
-
-                    likes.push(uid)
-                    evaluate.likes = [...likes]
-                }
-                await evaluate.save()
-
-                return new ServiceResponse(
-                    200,
-                    Status.SUCCESS,
-                    Messages.LIKE_OR_QUIT_LIKE_SUCCESS,
-                    evaluate
-                )
-            } else {
-
-                return new ServiceResponse(
-                    400,
-                    Status.ERROR,
-                    Messages.NOT_FOUND_DATA
-                )
-            }
-
-        } catch (err) {
-
-            return new ServiceResponse(
-                500,
-                Status.ERROR,
-                Messages.INTERNAL_SERVER
-            )
+        if (foundLike) {
+          const newLikes = likes.filter((l) => !l.equals(uid));
+          evaluate.likes = [...newLikes];
+        } else {
+          likes.push(uid);
+          evaluate.likes = [...likes];
         }
+        await evaluate.save();
+
+        return new ServiceResponse(
+          200,
+          Status.SUCCESS,
+          Messages.LIKE_OR_QUIT_LIKE_SUCCESS,
+          evaluate
+        );
+      } else {
+        return new ServiceResponse(400, Status.ERROR, Messages.NOT_FOUND_DATA);
+      }
+    } catch (err) {
+      return new ServiceResponse(500, Status.ERROR, Messages.INTERNAL_SERVER);
     }
+  }
 
-    async countByProductId(id) {
+  async countByProductId(id) {
+    try {
+      let evaluates = [];
+      await Promise.all([
+        Evaluate.count({
+          product: id,
+        }),
+        Evaluate.count({
+          product: id,
+          rate: 1,
+        }),
+        Evaluate.count({
+          product: id,
+          rate: 2,
+        }),
+        Evaluate.count({
+          product: id,
+          rate: 3,
+        }),
+        Evaluate.count({
+          product: id,
+          rate: 4,
+        }),
+        Evaluate.count({
+          product: id,
+          rate: 5,
+        }),
+      ]).then((result) => {
+        evaluates = {
+          total: result.shift(),
+          rate: [...result],
+        };
+      });
 
-        try {
+      return new ServiceResponse(
+        200,
+        Status.SUCCESS,
+        Messages.GET_DATA_SUCCESS,
+        evaluates
+      );
+    } catch (err) {
+      return new ServiceResponse(500, Status.ERROR, Messages.INTERNAL_SERVER);
+    }
+  }
 
-            let evaluates = []
-            await Promise.all([
-                Evaluate.count({
-                    product: id
-                }),
-                Evaluate.count({
-                    product: id,
-                    rate: 1
-                }),
-                Evaluate.count({
-                    product: id,
-                    rate: 2
-                }),
-                Evaluate.count({
-                    product: id,
-                    rate: 3
-                }),
-                Evaluate.count({
-                    product: id,
-                    rate: 4
-                }),
-                Evaluate.count({
-                    product: id,
-                    rate: 5
-                })
-            ]).then((result) => {
-                evaluates = {
-                    total: result.shift(),
-                    rate: [...result]
-                }
-            })
+  async getByUser(email) {
+    try {
+      const user = await User.findOne({ email }).exec();
 
-            return new ServiceResponse(
-                200,
-                Status.SUCCESS,
-                Messages.GET_DATA_SUCCESS,
-                evaluates
-            )
-        } catch (err) {
+      const evaluateList = await Evaluate.find({ user: user._id })
+        .populate("user")
+        .populate({
+          path: "product",
+          populate: {
+            path: "categoryId",
+            model: "Category",
+          },
+        })
+        .sort({ createdAt: -1 })
+        .exec();
 
-            return new ServiceResponse(
-                500,
-                Status.ERROR,
-                Messages.INTERNAL_SERVER
-            )
+      const evaluateDTOList = [];
+
+      evaluateList.forEach((evaluate) => {
+        const evaluateDTO = EvaluateDTO.mapEvaluateToEvaluateDTO(evaluate);
+        evaluateDTOList.push(evaluateDTO);
+      });
+
+      return new ServiceResponse(
+        200,
+        Status.SUCCESS,
+        Messages.GET_DATA_SUCCESS,
+        evaluateDTOList
+      );
+    } catch (err) {
+      return new ServiceResponse(500, Status.ERROR, Messages.INTERNAL_SERVER);
+    }
+  }
+
+  async getByProduct(id, sort, typeSort) {
+    try {
+      var evaluateList = [];
+
+      if (sort) {
+        if (sort === "desc") {
+          typeSort = 1;
         }
+        const findId = new mongoose.Types.ObjectId(id);
+        evaluateList = await Evaluate.aggregate([
+          {
+            $match: { product: findId },
+          },
+          {
+            $lookup: {
+              from: "users",
+              localField: "user",
+              foreignField: "_id",
+              as: "user",
+            },
+          },
+          {
+            $lookup: {
+              from: "products",
+              localField: "product",
+              foreignField: "_id",
+              as: "product",
+            },
+          },
+          {
+            $unwind: "$product",
+          },
+          {
+            $unwind: "$user",
+          },
+          {
+            $project: {
+              rate: 1,
+              comment: 1,
+              product: 1,
+              user: 1,
+              likes: { $ifNull: ["$likes", []] },
+              createdAt: 1,
+              likeCount: {
+                $cond: [{ $isArray: "$likes" }, { $size: "$likes" }, 0],
+              },
+            },
+          },
+          {
+            $sort: { likeCount: typeSort },
+          },
+        ]);
+      } else {
+        evaluateList = await Evaluate.find({ product: id })
+          .populate("user")
+          .populate({
+            path: "product",
+            populate: {
+              path: "categoryId",
+              model: "Category",
+            },
+          })
+          .sort({ updatedAt: -1 })
+          .exec();
+      }
+
+      const evaluateDToList = [];
+
+      evaluateList.forEach((evaluate) => {
+        const evaluateDTO = EvaluateDTO.mapEvaluateToEvaluateDTO(evaluate);
+
+        evaluateDToList.push(evaluateDTO);
+      });
+
+      return new ServiceResponse(
+        200,
+        Status.SUCCESS,
+        Messages.GET_DATA_SUCCESS,
+        evaluateDToList
+      );
+    } catch (err) {
+      console.log(err);
+      return new ServiceResponse(500, Status.ERROR, Messages.INTERNAL_SERVER);
     }
+  }
 
-    async getByUser(email) {
+  async insert(evaluate) {
+    try {
+      const product = await Product.findOne({ _id: evaluate.product }).exec();
 
-        try {
+      if (product) {
+        const user = await User.findOne({ _id: evaluate.user }).exec();
 
-            const user = await User.findOne({ email }).exec()
+        if (user) {
+          const newEvaluate = new Evaluate({ ...evaluate });
 
-            const evaluateList = await Evaluate.find({ user: user._id })
-                .populate("user")
-                .populate({
-                    path: "product",
-                    populate: {
-                        path: "categoryId",
-                        model: "Category",
-                    },
-                })
-                .sort({ createdAt: -1 })
-                .exec()
+          await newEvaluate.save();
 
-            const evaluateDTOList = []
-
-            evaluateList.forEach(evaluate => {
-
-                const evaluateDTO = EvaluateDTO.mapEvaluateToEvaluateDTO(evaluate)
-                evaluateDTOList.push(evaluateDTO)
-            })
-
-
-            return new ServiceResponse(
-                200,
-                Status.SUCCESS,
-                Messages.GET_DATA_SUCCESS,
-                evaluateDTOList
-            )
-
-        } catch (err) {
-
-            return new ServiceResponse(
-                500,
-                Status.ERROR,
-                Messages.INTERNAL_SERVER
-            )
+          return new ServiceResponse(
+            200,
+            Status.SUCCESS,
+            Messages.INSERT_EVALUATE_SUCCESS,
+            product
+          );
+        } else {
+          return new ServiceResponse(
+            400,
+            Status.ERROR,
+            Messages.NOT_FOUND_USER
+          );
         }
+      } else {
+        return new ServiceResponse(
+          400,
+          Status.ERROR,
+          Messages.NOT_FOUND_PRODUCT
+        );
+      }
+    } catch (err) {
+      console.log(err);
+      return new ServiceResponse(500, Status.ERROR, Messages.INTERNAL_SERVER);
     }
-
-    async getByProduct(id, sort, typeSort) {
-
-        try {
-
-            var evaluateList = []
-
-            if (sort) {
-
-                if (sort === 'desc') {
-                    typeSort = 1
-                }
-                const findId = new mongoose.Types.ObjectId(id)
-                evaluateList = await Evaluate.aggregate([
-                    {
-                        $match: { product: findId }
-                    },
-                    {
-                        $lookup: {
-                            from: "users",
-                            localField: "user",
-                            foreignField: "_id",
-                            as: "user"
-                        }
-                    },
-                    {
-                        $lookup: {
-                            from: "products",
-                            localField: "product",
-                            foreignField: "_id",
-                            as: "product"
-                        }
-                    },
-                    {
-                        $unwind: "$product"
-                    },
-                    {
-                        $unwind: "$user"
-                    },
-                    {
-                        $project: {
-                            rate: 1,
-                            comment: 1,
-                            product: 1,
-                            user: 1,
-                            likes: { $ifNull: ["$likes", []] },
-                            createdAt: 1,
-                            likeCount: { $cond: [{ $isArray: "$likes" }, { $size: "$likes" }, 0] }
-                        }
-                    },
-                    {
-                        $sort: { likeCount: typeSort }
-                    }
-                ])
-            } else {
-
-                evaluateList = await Evaluate.find({ product: id })
-                    .populate("user")
-                    .populate({
-                        path: "product",
-                        populate: {
-                            path: "categoryId",
-                            model: "Category",
-                        },
-                    }).sort({ updatedAt: -1 }).exec()
-            }
-
-            const evaluateDToList = []
-
-            evaluateList.forEach((evaluate) => {
-                const evaluateDTO = EvaluateDTO.mapEvaluateToEvaluateDTO(evaluate)
-
-                evaluateDToList.push(evaluateDTO)
-            })
-
-            return new ServiceResponse(
-                200,
-                Status.SUCCESS,
-                Messages.GET_DATA_SUCCESS,
-                evaluateDToList
-            )
-
-        } catch (err) {
-
-            console.log(err)
-            return new ServiceResponse(
-                500,
-                Status.ERROR,
-                Messages.INTERNAL_SERVER
-            )
-        }
-    }
-
-    async insert(evaluate) {
-
-        try {
-
-            const product = await Product.findOne({ _id: evaluate.product }).exec()
-
-            if (product) {
-
-                const user = await User.findOne({ _id: evaluate.user }).exec()
-
-                if (user) {
-                    const newEvaluate = new Evaluate({ ...evaluate })
-
-                    await newEvaluate.save()
-
-                    return new ServiceResponse(
-                        200,
-                        Status.SUCCESS,
-                        Messages.INSERT_EVALUATE_SUCCESS,
-                        product
-                    )
-                } else {
-                    return new ServiceResponse(
-                        400,
-                        Status.ERROR,
-                        Messages.NOT_FOUND_USER
-                    )
-                }
-            } else {
-                return new ServiceResponse(
-                    400,
-                    Status.ERROR,
-                    Messages.NOT_FOUND_PRODUCT
-                )
-            }
-
-        } catch (err) {
-
-            console.log(err)
-            return new ServiceResponse(
-                500,
-                Status.ERROR,
-                Messages.INTERNAL_SERVER
-            )
-        }
-    }
+  }
 }
 
-module.exports = new EvaluateService
+module.exports = new EvaluateService();
